2f896478432c570393e0f06c52895793c5806681
Eduarda Almeida <eduardapalm27@gmail.com>
2021-06-02 04:49:25 -0300

    adding new endpoints and making the original ones work

--- src/main/java/com/project/SantandersQuotationSystem/service/StockService.java
+++ src/main/java/com/project/SantandersQuotationSystem/service/StockService.java
@@ -1,11 +1,20 @@
 package com.project.SantandersQuotationSystem.service;
 
+import com.project.SantandersQuotationSystem.exceptions.BusinessException;
+import com.project.SantandersQuotationSystem.exceptions.BusinessException;
+import com.project.SantandersQuotationSystem.exceptions.NotFoundException;
 import com.project.SantandersQuotationSystem.mapper.StockMapper;
 import com.project.SantandersQuotationSystem.model.Stock;
 import com.project.SantandersQuotationSystem.model.dto.StockDTO;
 import com.project.SantandersQuotationSystem.repository.StockRepository;
+import com.project.SantandersQuotationSystem.util.MessageUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class StockService {
@@ -16,9 +25,50 @@ public class StockService {
     @Autowired
     private StockMapper mapper;
 
+    @Transactional
     public StockDTO save(StockDTO dto) {
+        Optional<Stock> optionalStock = repository.findByNameAndDate(dto.getName(), dto.getDate());
+        if(optionalStock.isPresent()){
+        throw new BusinessException(MessageUtils.STOCK_ALREADY_EXISTS);
+        }
+
         Stock stock = mapper.toEntity(dto);
         repository.save(stock);
+        return mapper.toDto(stock);
+    }
+
+
+
+    @Transactional
+    public StockDTO update(StockDTO dto) {
+        Optional<Stock> optionalStock = repository.findByStockUpdate(dto.getName(), dto.getDate(), dto.getId());
+        if(optionalStock.isPresent()){
+            throw new BusinessException(MessageUtils.STOCK_ALREADY_EXISTS);
+        }
+        Stock stock = mapper.toEntity(dto);
+        repository.save(stock);
+        return mapper.toDto(stock);
+
+    }
+
+    @Transactional
+    public StockDTO delete(Long id) {
+        StockDTO dto = this.findById(id);
+        repository.deleteById(dto.getId());
         return dto;
+    }
+
+    public List<StockDTO> findAll() {
+        return mapper.toDto(repository.findAll());
+    }
+
+    @Transactional(readOnly = true)
+    public StockDTO findById(Long id) {
+        return repository.findById(id).map(mapper::toDto).orElseThrow(NotFoundException::new);
+    }
+
+    @Transactional(readOnly = true)
+    public List<StockDTO> FindByToday() {
+        return repository.findByToday(LocalDate.now()).map(mapper::toDto).orElseThrow(NotFoundException::new);
     }
 }
