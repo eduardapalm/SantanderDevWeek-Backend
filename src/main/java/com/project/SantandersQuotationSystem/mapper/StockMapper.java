e0bc84478af8b064394be64db3480a25b7f8609f
Eduarda Almeida <eduardapalm27@gmail.com>
2021-06-02 02:26:04 -0300

    converting entities in dto

--- src/main/java/com/project/SantandersQuotationSystem/mapper/StockMapper.java
+++ src/main/java/com/project/SantandersQuotationSystem/mapper/StockMapper.java
@@ -16,4 +16,14 @@ public class StockMapper {
         stock.setDate(dto.getDate());
         return stock;
     }
+
+    public StockDTO toDto(Stock stock) {
+        StockDTO dto = new StockDTO();
+        dto.setId(stock.getId());
+        dto.setName(stock.getName());
+        dto.setPrice(stock.getPrice());
+        dto.setVariation(stock.getVariation());
+        dto.setDate(stock.getDate());
+        return dto;
+    }
 }
