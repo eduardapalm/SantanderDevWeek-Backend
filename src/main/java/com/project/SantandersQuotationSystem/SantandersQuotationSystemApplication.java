e1e74c748b74157e7804da443280c69a62cadfa9
Eduarda Almeida <eduardapalm27@gmail.com>
2021-06-02 04:48:06 -0300

    putting the new swagger description as a parameter

--- src/main/java/com/project/SantandersQuotationSystem/SantandersQuotationSystemApplication.java
+++ src/main/java/com/project/SantandersQuotationSystem/SantandersQuotationSystemApplication.java
@@ -3,6 +3,7 @@ package com.project.SantandersQuotationSystem;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.License;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -15,9 +16,9 @@ public class SantandersQuotationSystemApplication {
 	}
 
 	@Bean
-	public OpenAPI customOpenAPI(){
+	public OpenAPI customOpenAPI(@Value("${application.description}") String description){
 		return new OpenAPI().info(new Info()
-				.title("")
+				.title(description)
 				.version("1.0")
 				.termsOfService("http://swagger.io/terms")
 				.license(new License().name("Apache 2.0").url("http://springdoc.org"))
